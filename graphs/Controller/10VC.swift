4c3fbe797164d18f1a2e1e9b95a059bcffce9e7a
Danila Ferents <iambpev@gmail.com>
2020-03-03 11:40:20 +0300

    add 10 graph

---
+++ graphs/Controller/10VC.swift
@@ -0,0 +1,30 @@
+//
+//  10VC.swift
+//  graphs
+//
+//  Created by Danila Ferents on 25.02.20.
+//  Copyright © 2020 Danila Ferents. All rights reserved.
+//
+
+import UIKit
+
+class _0VC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
